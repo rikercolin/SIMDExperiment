5e8bddbd48c7c31b7cb011def206e2a9820a8da8
Colin Riker <rikercolin@gmail.com>
2021-03-25 12:28:19 -0400

    ImageBuilder implemented

---
+++ ImageBuilder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Threading;
+using System.Windows.Media;
+
+namespace SIMDExperiment
+{
+    class ImageBuilder
+    {
+        private readonly DirectoryInfo imageFolder;
+        private readonly int imageWidth;
+        private readonly int imageHeight;
+        private readonly int numberOfImages;
+
+        public ImageBuilder(DirectoryInfo folder, int width, int height, int number)
+        {
+            this.imageFolder = folder;
+            this.imageWidth = width;
+            this.imageHeight = height;
+            this.numberOfImages = number;
+        }
+
+        public void Build()
+        {
+            var countdown = new CountdownEvent(numberOfImages);
+            for (int i = 0; i < numberOfImages; i++)
+            {
+                ThreadPool.QueueUserWorkItem(x => {
+                    BuildTask(this);
+                    countdown.Signal();
+                });
+            }
+
+            while (countdown.CurrentCount > 0)
+            {
+                Thread.Sleep(100);
+                Console.Clear();
+                Console.WriteLine("Image creation in progress: {0}/{1}", numberOfImages, numberOfImages - countdown.CurrentCount);
+            }
+
+            Console.WriteLine("Image creation is done!");
+        }
+
+        private void BuildTask(object obj)
+        {
+            //Create the Structure of an image
+            PixelFormat pf = PixelFormats.Bgr32;
+            int rawStride = (this.imageWidth * pf.BitsPerPixel + 7) / 8;
+            byte[] rawImage = new byte[rawStride * this.imageHeight];
+
+            //Fill with random noise
+            Random random = new Random();
+            random.NextBytes(rawImage);
+
+            //Create the bitmap
+            BitmapSource bitmap = BitmapSource.Create(this.imageWidth, this.imageHeight, 96, 96, pf, null, rawImage, rawStride);
+            string name = Guid.NewGuid() + ".bmp";
+            string url = imageFolder.Name + "\\" + name;
+
+            //Write the bitmap
+            using (var filestream = new FileStream(url, FileMode.Create))
+            {
+                BitmapEncoder encoder = new BmpBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(bitmap));
+                encoder.Save(filestream);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+
 
 namespace SIMDExperiment
 {
     class Program
     {
+        const string imageFolderName = "testimages";
+        public static DirectoryInfo imageFolder;
+
         static void Main(string[] args)
         {
+            imageFolder = new DirectoryInfo(imageFolderName);
+
+
+            if (!imageFolder.Exists) System.IO.Directory.CreateDirectory(imageFolderName);
+
+            ImageBuilder imageBuilder = new ImageBuilder(imageFolder, 100, 100, 1000);
+            imageBuilder.Build();
+
+            Console.WriteLine("Enter any key to start deleting");
+            Console.ReadLine();
+
+            foreach (FileInfo file in imageFolder.GetFiles())
+            {
+                file.Delete();
+            }
+            
         }
     }
+
+   
 }
